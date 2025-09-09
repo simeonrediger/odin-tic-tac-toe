686b6a0a74464d0606241b59c4bea000623d759a
Simeon Rediger <simeonrediger@gmail.com>
2025-09-09 01:13:58 -0400

    Add player number getter

--- script.js
+++ script.js
@@ -69,6 +69,7 @@ function createPlayer(name) {
 
     return {
         getName: () => name,
+        getPlayerNumber: () => playerNumber,
         setPlayerNumber: number => playerNumber = number,
     };
 }
